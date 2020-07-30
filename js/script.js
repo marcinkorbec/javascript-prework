4a9e594d2d645e9c70248f00000b5539a8d9fcea
Marcin Korbecki <marcinkorbecki.dw@gmail.com>
2020-07-30 23:45:35 +0200

    Add buttons

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // computer
-
-
+function playGame(playerInput){
+    clearMessages()
     let randomNumber = Math.floor(Math.random() * 3 + 1);
     console.log('Wylosowana liczba to: ' + randomNumber);
 
@@ -26,7 +26,7 @@
 
 
     // player
-    let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+    //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
     console.log('Gracz wpisał: ' + playerInput);
 
@@ -79,5 +79,16 @@
                 printMessage('REMIS!');
             }
         }
+}
 
+document.getElementById('play-rock').addEventListener('click', function() {
+    playGame(1);
+  });
 
+document.getElementById('play-paper').addEventListener('click', function() {
+    playGame(2);
+  });
+
+document.getElementById('play-scissors').addEventListener('click', function() {
+    playGame(3);
+  });
